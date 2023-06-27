9352c6de8a96e434896de5d7384fd060ce74a065
AlaaAyaad74 <alaaayad1@outlook.com>
2023-06-27 22:12:36 +0300

    dashboard 4th commit

--- app/dashboard_/page.js
+++ app/dashboard_/page.js
@@ -24,7 +24,7 @@ function Dashboard() {
             </li>
           </ul>
         </div> */}
-        <Asidedashboard />
+      
         <div className="container">
           <div
             style={{
