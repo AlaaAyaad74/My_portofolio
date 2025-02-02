6ed844a99f38a6c1d1a42d6cc736bc949937b094
alaagonovo <alaa@gonovo.com>
2025-02-02 14:07:10 +0200

    adding scaffolding

--- app/my_works/MyWorksData.js
+++ app/my_works/MyWorksData.js
@@ -1,43 +1,48 @@
 export default [
   {
+    name: "Scaffolding",
+    image: "./scaffolding.webp",
+    link: "https://thescaffolding.co.uk/",
+  },
+  {
     name: "Bling Weddings",
     image: "/bling.webp",
     link: "https://www.bling-weddings.com/",
   },
   {
     name: "Click Shop",
-    image: "./clickshop.png",
+    image: "./clickshop.webp",
     link: "https://e-commerce-click-an35.vercel.app/",
   },
   {
     name: "Land Interiors",
-    image: "./coberg.png",
+    image: "./coberg.webp",
     link: "https://cobirg-task.vercel.app/",
   },
   {
     name: "Iphone 15",
-    image: "./i-phone15.PNG",
+    image: "./i-phone15.webp",
     link: "https://i-phone15-theta.vercel.app/",
   },
-  {
-    name: "National Livre Institute",
-    image: "./institute.PNG",
-    link: "https://nli.menofia.edu.eg/en",
-  },
+  // {
+  //   name: "National Livre Institute",
+  //   image: "./institute.webp",
+  //   link: "https://nli.menofia.edu.eg/en",
+  // },
   {
     name: "Tesla Clone",
-    image: "./Tesla.PNG",
+    image: "./Tesla.webp",
     link: "https://tesla-clone-lovat.vercel.app/",
   },
 
   {
     name: "Amazon Clone",
-    image: "./amazon.png",
+    image: "./amazon.webp",
     link: "https://amazon-clone-psi-neon.vercel.app/",
   },
   {
     name: "To Do List",
-    image: "./todolist.PNG",
+    image: "./todolist.webp",
     link: "https://todo-list-three-woad.vercel.app/",
   },
 ];
